9fda907405ac47b8116e377fd1ec70589501d289
Facundo Pellicer <facundopellicer4@gmail.com>
2023-01-17 14:56:55 -0300

    remove fnc

--- src/utils/ranking.ts
+++ src/utils/ranking.ts
@@ -1,17 +1,6 @@
 import axios from "axios";
 import { Dispatch, SetStateAction } from "react";
 import { UsersRanking } from "../types/ranking";
-import { GameOver } from "../types/socketio";
-import { fetchGameRanking } from "./fetching";
-
-export const updateGameRankingState = async (
-  setUsersRanking: Dispatch<SetStateAction<UsersRanking[]>>,
-  data: GameOver
-) => {
-  const ranking = await fetchGameRanking(data.id);
-
-  setUsersRanking(ranking);
-};
 
 export const getGameRanking = async (
   id: string,
